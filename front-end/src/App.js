e977a025d7481a4008f4a662ea4f23689458f42e
pg <86490119+pruyyanshu1@users.noreply.github.com>
2023-06-16 11:02:48 +0530

    done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import preNav from './components/preNav';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import PreNav from './components/PreNav';
+import Slider from './components/Slider';
+import data from './data/data.json';
+import Offers from './components/Offers';
+
 function App() {
   return (
-    <div>
-     <preNav/>
-    </div>
+    <Router>
+      <PreNav />
+      <Navbar />
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+    </Router>
   );
 }
 
--- front-end/src/components/preNav.js
+++ front-end/src/components/preNav.js
@@ -1,18 +1,24 @@
 import React from 'react'
-import "../styles/preNav.css";
+import "../styles/PreNav.css";
 
+const carticon = <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M286.788-81Q257-81 236-102.212q-21-21.213-21-51Q215-183 236.212-204q21.213-21 51-21Q317-225 338-203.788q21 21.213 21 51Q359-123 337.788-102q-21.213 21-51 21Zm400 0Q657-81 636-102.212q-21-21.213-21-51Q615-183 636.212-204q21.213-21 51-21Q717-225 738-203.788q21 21.213 21 51Q759-123 737.788-102q-21.213 21-51 21ZM235-741l110 228h288l125-228H235Zm-30-60h589.074q22.964 0 34.945 21Q841-759 829-738L694-495q-11 19-28.559 30.5Q647.881-453 627-453H324l-56 104h491v60H277q-42 0-60.5-28t.5-63l64-118-152-322H51v-60h117l37 79Zm140 288h288-288Z"/></svg>
 
-const preNav = () => {
+const PreNav = () => {
   return (
     <div className='preNav'>
         <div>
-            <a href=''>MI INDIA</a><span>|</span>
-            <a href=''>Get MI Store App</a><span>|</span>
-            <a href=''>Online Help</a><span>|</span>
-            <a href=''>Retail Store</a>
+            <a href='https://www.mi.com/in'>MI INDIA</a><span>|</span>
+            <a href='https://www.mi.com/in'>Get MI Store App</a><span>|</span>
+            <a href='https://www.mi.com/in'>Online Help</a><span>|</span>
+            <a href='https://www.mi.com/in'>Retail Store</a>
+        </div>
+        <div>
+            <a href='https://www.mi.com/in'>Login</a><span>|</span>
+            <a href='https://www.mi.com/in'>Register</a><span>|</span>
+            <a href='https://www.mi.com/in'>{carticon}Cart (0)</a>
         </div>
     </div>
   )
 }
 
-export default preNav
+export default PreNav
