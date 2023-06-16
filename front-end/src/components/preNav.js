8451c321ea77cfe887bcdcc10dcd680726e82643
pg <86490119+pruyyanshu1@users.noreply.github.com>
2023-06-16 11:02:22 +0530

    all proper

---
+++ front-end/src/components/preNav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../styles/preNav.css";
+
+
+const preNav = () => {
+  return (
+    <div className='preNav'>
+        <div>
+            <a href=''>MI INDIA</a><span>|</span>
+            <a href=''>Get MI Store App</a><span>|</span>
+            <a href=''>Online Help</a><span>|</span>
+            <a href=''>Retail Store</a>
+        </div>
+    </div>
+  )
+}
+
+export default preNav
